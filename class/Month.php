10a254c80ebef19e1e0970416b3f2c560998d564
duiliopastorelli <info@duiliopastorelli.com>
2016-05-28 21:07:23 +0100

    Italian months added

--- class/Month.php
+++ class/Month.php
@@ -2,28 +2,114 @@
 
 class Month
 {
-    public static function getMonth($lang = null, $tMachine=null)
+    /*
+     * get the month.
+     * 1st parameter is the language (it, de, es, ...)
+     * 2nd parameter is the month (3->March, 4->April, ...)
+     * 3rd parameter is the timeMachine (-1->previous month, 1->next month, ...)
+     */
+    public static function getMonth($lang=null, $month=null, $tMachine=null)
     {
-        //Add or remove an integer to generate future or past months
-        if($tMachine!==0){
-            $result = self::generateMonth()+$tMachine;
+        if (isset($month)){
+            $requestedMonth = $month;
+        } else {
+            $requestedMonth = self::timeMachine($tMachine);
         }
 
         //Switch between languages to generate a proper string ready for output
         switch ($lang){
-            case "zh":
-                return self::generateChineseMonth($result);
+            case "it":
+                $result = self::generateItalianMonth($requestedMonth);
             break;
+                
+            case "zh":
+                $result = self::generateChineseMonth($requestedMonth);
+            break;
+            
+            default:
+                $result = $requestedMonth;
         }
         return $result;
     }
 
     //Generate a mounth using timezone UTC and 1->12 numbers without the zero
-    private function generateMonth()
+    private function generateCurrentMonth()
     {
         date_default_timezone_set('UTC');
         
         return date("n");
+    }
+    
+    //Generate a mounth number adding o removing a vertain number of months
+    private function timeMachine($montsDiff)
+    {
+        if (!isset($montsDiff)){
+            return self::generateCurrentMonth();
+        } else {
+            $currentMonth = self::generateCurrentMonth();
+            if (($currentMonth+$montsDiff)>12){
+                return ($currentMonth+$montsDiff)-12;
+            } else {
+                return $currentMonth+$montsDiff;
+            }
+        }
+    }
+    
+    private function generateItalianMonth($generatedMonth)
+    {
+        switch ($generatedMonth){
+            case 1:
+                return "gennaio";
+            break;
+            
+            case 2:
+                return "febbraio";
+            break;
+
+            case 3:
+                return "marzo";
+                break;
+
+            case 4:
+                return "aprile";
+                break;
+
+            case 5:
+                return "maggio";
+                break;
+
+            case 6:
+                return "giugno";
+                break;
+
+            case 7:
+                return "luglio";
+                break;
+
+            case 8:
+                return "agosto";
+                break;
+
+            case 9:
+                return "settembre";
+                break;
+
+            case 10:
+                return "ottobre";
+                break;
+
+            case 11:
+                return "novembre";
+                break;
+
+            case 12:
+                return "dicembre";
+                break;
+
+            case 1:
+                return "gennaio";
+                break;
+        }
     }
 
     //Generate The Chinese month ready to output, adding the Chinese character
--- tests/MonthTest.php
+++ tests/MonthTest.php
@@ -8,21 +8,55 @@ class MonthTest extends PHPUnit_Framework_TestCase
         return date("n");
     }
 
-    public function testGetDefaultPhpMonth()
+    public function testGetCurrentPhpMonth()
     {
         $this->assertEquals(self::currentMonth(), Month::getMonth());
-    }
-
-    public function testGetChineseMonth()
-    {
-        $this->assertEquals("5月", Month::getMonth("zh"));
     }
 
     public function testGetNextMonth()
     {
         $month = self::currentMonth();
         $month++;
-        $this->assertEquals($month, Month::getMonth(null,1));
+        $this->assertEquals($month, Month::getMonth(null, null, 1));
     }
 
+    public function testGetPrevtMonth()
+    {
+        $month = self::currentMonth();
+        $month--;
+        $this->assertEquals($month, Month::getMonth(null, null, -1));
+    }
+
+    public function testGetItalianMonths()
+    {
+        $this->assertEquals("maggio", Month::getMonth("it"));
+        $this->assertEquals("gennaio", Month::getMonth("it", 1));
+        $this->assertEquals("febbraio", Month::getMonth("it", 2));
+        $this->assertEquals("marzo", Month::getMonth("it", 3));
+        $this->assertEquals("aprile", Month::getMonth("it", 4));
+        $this->assertEquals("maggio", Month::getMonth("it", 5));
+        $this->assertEquals("giugno", Month::getMonth("it", 6));
+        $this->assertEquals("luglio", Month::getMonth("it", 7));
+        $this->assertEquals("agosto", Month::getMonth("it", 8));
+        $this->assertEquals("settembre", Month::getMonth("it", 9));
+        $this->assertEquals("ottobre", Month::getMonth("it", 10));
+        $this->assertEquals("novembre", Month::getMonth("it", 11));
+        $this->assertEquals("dicembre", Month::getMonth("it", 12));
+    }
+    
+    public function testGetItalianPrevMonth()
+    {
+        $this->assertEquals("aprile", Month::getMonth("it",null, -1));
+    }
+    
+    public function testGetItalianNextMonth()
+    {
+        $this->assertEquals("giugno", Month::getMonth("it",null, +1));
+    }
+
+    public function testGetChineseMonth()
+    {
+        $this->assertEquals("5月", Month::getMonth("zh"));
+    }
+    
 }
